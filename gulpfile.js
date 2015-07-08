60585023db1e6bbb4b2f034eaae20f2319aa9b23
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-07-08 16:39:25 -0500

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var bs = require('browser-sync');
+var reload = bs.reload;
+
+
+gulp.task('serve', ['browser-sync', 'jade'], function () {
+    gulp.watch('views/**/*.jade', gulp.run('jade'));
+
+    gulp.watch('views/**/*.html').on('change', bs.reload);
+    bs.watch('routes/**/*.js').on('change', function () {
+        setTimeout(function reload() {
+            bs.reload({
+                stream: false   //
+            });
+        }, 500);
+    });
+
+
+});
+
+gulp.task('jade', function () {
+    return gulp.src('views/**/*.jade')
+        .pipe($.jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('views/'))
+})
+
+gulp.task('browser-sync', ['nodemon'], function () {
+    bs.init(null, {
+        proxy: "http://localhost:5000",
+        browser: "google chrome",
+        delay: 1000,
+        notify: false,
+        port: 7000,
+    });
+});
+gulp.task('nodemon', function (cb) {
+
+    var started = false;
+
+    return $.nodemon({
+        script: 'app.js'
+    }).on('start', function () {
+        if (!started) {
+            cb();
+            started = true;
+        }
+    }, bs.reload)
+        .on('restart', function onRestart() {
+            // reload connected browsers after a slight delay
+            setTimeout(function reload() {
+                bs.reload({
+                    stream: false   //
+                });
+            }, 500);
+        });
+    ;
+});
