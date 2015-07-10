67e4c9dc8a36c82480e7023bd27eafb8b3424ac7
ChicagoBusinessIntelligence1 <Ytrewq12>
2015-07-10 10:58:27 -0500

    Flower

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 'use strict'
 angular.module('app', ['firebase','ngSanitize']).controller('MainCtrl', function ($scope, $firebaseObject) {
-    var ref = new Firebase('https://sv-app-test.firebaseio.com/articles/-JsRqcUVbvFCNBI7Dr3Q');
+    var ref = new Firebase('https://sv-app-test.firebaseio.com/articles/-JrncbQ7s8q8Em6-myq-');
 
     var obj = $firebaseObject(ref);
     obj.$loaded().then(function (data) {
