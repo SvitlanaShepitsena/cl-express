74958cfe8033e08604d0ea72ca1997f00af82e1a
chicagobusinessintelligence2@gmail.com <Ytrewq12>
2015-07-10 15:11:08 -0500

    Commitment

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var Firebase = require("firebase");
 
 var app = express();
 
+app.locals.pretty = true;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
